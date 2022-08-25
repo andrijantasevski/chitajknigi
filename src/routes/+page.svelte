ff3c6d9622a56987d637ce6701c9fc81e7c56efa
Andrijan Tasevski <andrijantasevski@gmail.com>
2022-08-25 15:04:53 +0200

    Reading books section added. When form is submitted, a new item shows up with the name of the book and a delete button (not functional yet). Fixed RegEx to allow spaces between words.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,7 @@
 	import Favicon from '../lib/Favicon.svelte';
 	import Header from '../lib/Header.svelte';
 	import AddBookForm from '../lib/AddBookForm.svelte';
+	import ReadBooksSection from '../lib/ReadBooksSection.svelte';
 </script>
 
 <MetaTags />
@@ -11,4 +12,5 @@
 <Header />
 <main>
 	<AddBookForm />
+	<ReadBooksSection />
 </main>
